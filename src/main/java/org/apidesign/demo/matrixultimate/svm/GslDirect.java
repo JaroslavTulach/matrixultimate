c10a2c07dee074737e4f19eee40ccc9710c123a0
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2018-09-04 16:50:36 +0200

    Direct calls to @CFunction doesn't make it faster

--- src/main/java/org/apidesign/demo/matrixultimate/svm/GslDirect.java
+++ src/main/java/org/apidesign/demo/matrixultimate/svm/GslDirect.java
@@ -1,6 +1,5 @@
 package org.apidesign.demo.matrixultimate.svm;
 
-import org.apidesign.demo.matrixultimate.GreatScientificLibrary;
 import org.graalvm.nativeimage.c.CContext;
 import org.graalvm.nativeimage.c.function.CEntryPoint;
 import org.graalvm.nativeimage.c.function.CFunction;
@@ -8,30 +7,29 @@ import org.graalvm.nativeimage.c.struct.CField;
 import org.graalvm.nativeimage.c.struct.CStruct;
 import org.graalvm.word.Pointer;
 import org.graalvm.word.PointerBase;
-import org.graalvm.word.WordFactory;
 import org.apidesign.demo.matrixultimate.MatrixSearch;
+import org.graalvm.word.WordFactory;
 
 @CContext(GslDirectives.class)
-final class GslDirect implements GreatScientificLibrary<GslDirect.GslMatrix> {
+final class GslDirect {
     @CStruct("gsl_matrix")
     static interface GslMatrix extends PointerBase {
         @CField long size1();
         @CField long size2();
     }
 
-    private static final GslDirect GSL = new GslDirect();
     private static final MatrixSearch FIND_BIGGEST_SQUARE = new FindBiggestSquare();
 
     static final class FindBiggestSquare implements MatrixSearch {
         @Override
         public MatrixSearch.Result search(long matrixPtr) {
-            GslMatrix matrix = GSL.fromRaw(matrixPtr);
+            GslMatrix matrix = WordFactory.pointer(matrixPtr);
 
             long stamp = System.currentTimeMillis();
 
-            final long size1 = GSL.getSize1(matrix);
-            final long size2 = GSL.getSize2(matrix);
-            GslMatrix sizes = GSL.create(size1, size2);
+            final long size1 = matrix.size1();
+            final long size2 = matrix.size2();
+            GslMatrix sizes = gsl_matrix_alloc(size1, size2);
 
             long max = 0;
             long row = -1;
@@ -39,15 +37,15 @@ final class GslDirect implements GreatScientificLibrary<GslDirect.GslMatrix> {
 
             for (long i = size1 - 1; i >= 0; i--) {
                 for (long j = size2 - 1; j >= 0; j--) {
-                    double v00 = GSL.get(matrix, i, j);
-                    double v01 = i == size1 - 1 ? -1 : GSL.get(matrix, i + 1, j);
-                    double v10 = j == size2 - 1 ? -1 : GSL.get(matrix, i, j + 1);
-                    double v11 = i == size1 - 1 || j == size2 - 1 ? -1 : GSL.get(matrix, i + 1, j + 1);
+                    double v00 = gsl_matrix_get(matrix, i, j);
+                    double v01 = i == size1 - 1 ? -1 : gsl_matrix_get(matrix, i + 1, j);
+                    double v10 = j == size2 - 1 ? -1 : gsl_matrix_get(matrix, i, j + 1);
+                    double v11 = i == size1 - 1 || j == size2 - 1 ? -1 : gsl_matrix_get(matrix, i + 1, j + 1);
 
                     if (v00 == v01 && v10 == v11 && v00 == v11) {
-                        double s10 = GSL.get(sizes, i + 1, j);
-                        double s01 = GSL.get(sizes, i, j + 1);
-                        double s11 = GSL.get(sizes, i + 1, j + 1);
+                        double s10 = gsl_matrix_get(sizes, i + 1, j);
+                        double s01 = gsl_matrix_get(sizes, i, j + 1);
+                        double s11 = gsl_matrix_get(sizes, i + 1, j + 1);
 
                         double min = s10;
                         if (min > s01) {
@@ -57,18 +55,18 @@ final class GslDirect implements GreatScientificLibrary<GslDirect.GslMatrix> {
                             min = s11;
                         }
 
-                        GSL.set(sizes, i, j, min + 1);
+                        gsl_matrix_set(sizes, i, j, min + 1);
                         if (max <= min) {
                             row = i;
                             column = j;
                             max = (long) min + 1;
                         }
                     } else {
-                        GSL.set(sizes, i, j, 1.0);
+                        gsl_matrix_set(sizes, i, j, 1.0);
                     }
                 }
             }
-            GSL.free(sizes);
+            gsl_matrix_free(sizes);
 
             long took = System.currentTimeMillis() - stamp;
             return new MatrixSearch.Result(row, column, max, took);
@@ -124,45 +122,5 @@ final class GslDirect implements GreatScientificLibrary<GslDirect.GslMatrix> {
         MatrixSearch.Result result = FIND_BIGGEST_SQUARE.search(ptr.rawValue());
         System.err.printf("  ditto via native code took %d ms\n", result.getMilliseconds());
         return result.hashCode();
-    }
-
-    @Override
-    public GslMatrix create(long size1, long size2) {
-        return gsl_matrix_alloc(size1, size2);
-    }
-
-    @Override
-    public void free(GslMatrix matrix) {
-        gsl_matrix_free(matrix);
-    }
-
-    @Override
-    public long toRaw(GslMatrix m) {
-        return m.rawValue();
-    }
-
-    @Override
-    public GslMatrix fromRaw(long m) {
-        return WordFactory.pointer(m);
-    }
-
-    @Override
-    public double get(GslMatrix matrix, long i, long j) {
-        return gsl_matrix_get(matrix, i, j);
-    }
-
-    @Override
-    public void set(GslMatrix matrix, long i, long j, double v) {
-        gsl_matrix_set(matrix, i, j, v);
-    }
-
-    @Override
-    public long getSize1(GslMatrix matrix) {
-        return matrix.size1();
-    }
-
-    @Override
-    public long getSize2(GslMatrix matrix) {
-        return matrix.size2();
     }
 }
